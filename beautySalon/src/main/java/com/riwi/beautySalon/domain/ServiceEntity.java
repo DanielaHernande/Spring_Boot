33194016a648656d03be310a65765d80bbf65842
Daniela_J_H <154357197+DanielaHernande@users.noreply.github.com>
2024-05-06 13:18:05 -0500

    Add files via upload

---
+++ beautySalon/src/main/java/com/riwi/beautySalon/domain/ServiceEntity.java
@@ -0,0 +1,51 @@
+package com.riwi.beautySalon.domain;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity(name = "service")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ServiceEntity {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 100, nullable = false)
+    private String name;
+
+    @Lob // -> Mapea a TEXT. Declarar el atributo como tipo TEXT
+    private String description;
+
+    @Column(nullable = false)
+    private BigDecimal price;
+
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    @OneToMany(
+        fetch = FetchType.EAGER,
+        mappedBy = "service",
+        cascade = CascadeType.ALL,
+        orphanRemoval = false
+    )
+    private List<Appointment> appointments;
+}
